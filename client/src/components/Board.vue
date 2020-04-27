50ca6ffe44bc25024ef998be76a2373eb6c5cc2d
vincent plouchard <vincent.plouchard@epfedu.fr>
2020-04-27 17:24:03 +0200

    Table no css

--- client/src/components/Board.vue
+++ client/src/components/Board.vue
@@ -3,43 +3,29 @@
     <h1>Board</h1>
 
     <!-- Board -->
-    <v-row>
-      <!-- Left part of the Board -->
-      <v-col class="cardleft" cols="11" md="9" lg="9" xl="9">
-        <v-row justify="end">
-          <!-- Dimensions -->
-          <v-col
-            v-for="(data, i) in datas"
-            :key="i"
-            cols="11"
-            :md="`${lenData}`"
-            :lg="`${lenData}`"
-            :xl="`${lenData}`"
-            class="table pb-0"
-          >
-            <p>{{ data.value }}</p>
-            <v-row align="end" justify="center">
-              <!-- Levels -->
-              <v-col
-                v-for="(data, j) in datas[i].level"
-                :key="j"
-                cols="11"
-                :md="`${lenlevels(i)}`"
-                :lg="`${lenlevels(i)}`"
-                :xl="`${lenlevels(i)}`"
-                class="level withoutpad"
-                ><p>{{ data }}</p>
-              </v-col>
-            </v-row>
-          </v-col>
-        </v-row>
-      </v-col>
+    <table>
+      <thead>
+        <!-- do a th for dimension with a for loop -->
+        <!-- do a tr for levels with a for loop inside th -->
+        <!-- do th dfor Mesure -->
+        <th v-for="(data, i) in datas" :key="i">
+          {{ data.value | capitalize }}
 
-      <!-- Right part of the Board -->
-      <v-col class="card" cols="11" md="3" lg="3" xl="3">
-        <p>{{ mesure }}</p>
-      </v-col>
-    </v-row>
+          <tr>
+            <th v-for="(level, j) in datas[i].level" :key="j">
+              {{ level | capitalize }}
+            </th>
+          </tr>
+        </th>
+
+        <th>
+          {{ mesure | capitalize }}
+        </th>
+      </thead>
+      <tbody>
+        <!-- fill of jtext fields -->
+      </tbody>
+    </table>
   </div>
 </template>
 
