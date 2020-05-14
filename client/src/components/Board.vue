f0aca8e398cf74a73b409305f21ea3415273337a
vincent plouchard <vincent.plouchard@epfedu.fr>
2020-05-14 23:42:03 +0200

    add popup on delete button

--- client/src/components/Board.vue
+++ client/src/components/Board.vue
@@ -1,7 +1,6 @@
 <template>
   <div justify="center">
     <h1>Board</h1>
-
     <!-- Board -->
     <table v-if="datas.length > 0 || mesure.length > 0">
       <thead>
@@ -24,22 +23,24 @@
       <tbody>
         <!-- fill of jtext fields -->
         <tr v-for="k in nbline" :key="k">
-          <td v-for="level in levels" :key="level">
+          <td v-for="(level, j) in levels" :key="level">
             <v-text-field
+              :id="'textfield:' + k + ';' + (j + 1)"
               label="Entrez vos données"
-              single-line
               solo
               flat
               hide-details="True"
+              @change="insertTab"
             ></v-text-field>
           </td>
           <td>
             <v-text-field
+              :id="'textfield:' + k + ';' + (levels.length + 1)"
               label="Entrez vos données"
-              single-line
               solo
               flat
               hide-details="True"
+              @change="insertTab"
             ></v-text-field>
           </td>
         </tr>
@@ -50,7 +51,10 @@
             x-small
             color="light-blue accent-3"
             class="ma-2 white--text"
-            @click="addLign()"
+            @click="
+              insertTab();
+              addLign();
+            "
             :disabled="nbline > 14"
           >
             Ajouter une ligne
@@ -61,10 +65,39 @@
             x-small
             color="red accent-3"
             class="ma-2 white--text"
-            @click="delLign()"
+            @click="
+              insertTab();
+              verifLastLign();
+            "
             :disabled="nbline < 4"
             >Suprimer une ligne</v-btn
           >
+          <v-dialog v-model="dialog" persistent max-width="350">
+            <v-card>
+              <v-card-title class="headline">Attention !</v-card-title>
+              <v-card-text>
+                <p>Un champs de la dernière ligne n'est pas vide.</p>
+                <p class="font-weight-bold">
+                  Êtes vous sûr de vouloir supprimer la ligne?
+                </p>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                  color="red darken-1"
+                  text
+                  @click="
+                    dialog = false;
+                    delLign();
+                  "
+                  >Suprimer</v-btn
+                >
+                <v-btn color="light-blue darken-1" text @click="dialog = false"
+                  >Annuler</v-btn
+                >
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
         </td>
       </tfoot>
     </table>
@@ -89,9 +122,7 @@ export default {
       this.refresh;
       var levels = [];
       for (var i = 0; i < this.datas.length; i++) {
-        console.log("dim");
         for (var j = 0; j < this.datas[i].level.length; j++) {
-          console.log("lev");
           levels.push(this.datas[i].level[j]);
         }
       }
@@ -100,7 +131,9 @@ export default {
   },
   data() {
     return {
+      dialog: false,
       nbline: 3,
+      textFieldsValues: [],
     };
   },
   methods: {
@@ -109,6 +142,32 @@ export default {
     },
     delLign() {
       return (this.nbline -= 1);
+    },
+    verifLastLign() {
+      console.log(
+        this.textFieldsValues[this.textFieldsValues.length - 1].join("")
+      );
+      if (
+        this.textFieldsValues[this.textFieldsValues.length - 1].join("") != ""
+      ) {
+        this.dialog = true;
+      } else {
+        this.delLign();
+      }
+    },
+    insertTab: function() {
+      var insertTab = [];
+      [];
+      for (var i = 1; i <= this.nbline; i++) {
+        var lign_value = [];
+        for (var j = 1; j <= this.levels.length + 1; j++) {
+          var str_id = `textfield:${i};${j}`;
+          lign_value.push(document.getElementById(str_id).value);
+        }
+        insertTab.push(lign_value);
+      }
+      this.textFieldsValues = insertTab;
+      return this.textFieldsValues;
     },
   },
 };
@@ -130,7 +189,6 @@ table {
 tfoot td {
   border: none;
 }
-
 th {
   background-color: #1976d2;
   color: white;
